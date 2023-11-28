935b4435a1766e63e377332e66f10f5e25b562c6
jharry444 <jharrison1@qa.com>
2023-11-28 10:08:53 +0000

    json example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Trainer from './components/Trainer';
+import trainers from './data/trainers.json';
 
 // react component called App
 function App() {
+  console.log("Trainers:", trainers);
+
   return (
     <div>
       <header></header>
@@ -12,7 +15,7 @@ function App() {
       <h1>React App</h1>
       <p>hello</p>
       <p>world</p>
-      <Trainer name="Jordan H" age="29" specialism="software"/>
+      <Trainer/>
       <Trainer name="Andy" age="21" specialism="software"/>
       <Trainer name="Elliot" age="27" specialism="software"/>
       <Trainer />
